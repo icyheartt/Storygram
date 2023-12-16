e438598f1eee6fc6980fb29f440e6ec81271dfb3
icyheartt <qorcksrb9131@gmail.com>
2023-12-17 03:40:14 +0900

    storygram

--- app/src/main/java/com/example/home33/storyadd.kt
+++ app/src/main/java/com/example/home33/storyadd.kt
@@ -27,7 +27,7 @@ import java.io.IOException
 
 
 @Suppress("DEPRECATION")
-class Storyadd : AppCompatActivity() {
+class storyadd : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     var photouri : Uri? = null
     var photoResult = registerForActivityResult(StartActivityForResult()){
