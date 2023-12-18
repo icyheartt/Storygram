0125ee62fe290cb79fa836b875285eb10a47fdc8
icyheartt <qorcksrb9131@gmail.com>
2023-12-18 16:13:50 +0900

    storygram

--- app/src/main/java/com/example/home33/storyadd.kt
+++ app/src/main/java/com/example/home33/storyadd.kt
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
+import android.text.Spannable
+import android.text.Spanned
+import android.text.style.RelativeSizeSpan
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.Button
@@ -20,7 +23,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
-import com.greenfrvr.hashtagview.HashtagView
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -52,11 +54,53 @@ class storyadd : AppCompatActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
-
-
-
     }
 
+    private fun tag(editText: EditText) :ArrayList<String>{
+        val inputText = editText.text.toString()
+        var main_result = inputText
+
+        var copy = "$inputText "		// 끝에 공백 추가 안하면 인덱스 벗어남;;
+        val span: Spannable = main_result as Spannable
+        val result = ArrayList<String>()		// 태그값을 저장할 배열
+        var count = 0		// 태그 갯수
+        var startIndex = 0		// 각 태그의 시작 인덱스를 저장할 변수
+
+        for (element in copy) {		// 1
+            if (element == '#') count++
+        }
+
+        for (i in 0 until count) {		// 2
+            for (j in copy) startIndex = copy.indexOf('#')		// 3
+
+            try {
+                result.add(		// 4
+                    copy.substring(
+                        startIndex,
+                        (copy.substring(startIndex).indexOf(" ") + startIndex)
+                    )
+                )
+
+                // 5
+                copy = copy.substring((copy.substring(startIndex).indexOf(" ") + startIndex) + 1)
+            } catch (e: Exception) {
+                println(e.message)
+                Toast.makeText(this, "#를 붙이지 마세요", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        for (i in result) {		// 태그만 가지고 스팬 적용
+            span.setSpan(
+                RelativeSizeSpan(1.5f),
+                main_result.toString().indexOf(i),
+                main_result.toString().indexOf(i) + i.length,
+                Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
+        }
+        return result
+    }
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.storyadd)
@@ -67,7 +111,7 @@ class storyadd : AppCompatActivity() {
         var next = findViewById<Button>(R.id.Storyadd_Button_next)
         var calendar1 = findViewById<CalendarView>(R.id.Storyadd_CalendarView_calendar)
         var time1 = findViewById<TimePicker>(R.id.Storyadd_TimePicker_time)
-        var addtag = findViewById<HashtagView>(R.id.Storyadd_HashtagView_addtag)
+        var addtag = findViewById<EditText>(R.id.add_tag)
         var previous = findViewById<Button>(R.id.Storyadd_Button_previous)
 
         btnBack2.setOnClickListener {
@@ -101,7 +145,7 @@ class storyadd : AppCompatActivity() {
                         })
                     val dpHelper = DBHelper(this)
                     //여기에 dbHelper 메소드 삽입
-                    val item = StoryItem(comment.toString(), calendar1.date.toString(), imagePath, )
+                    val item = StoryItem(comment.toString(), calendar1.date.toString(), imagePath, tag(addtag))
                     dpHelper.insertStory(item)
                     finish()
                 } catch (e: Exception) {
